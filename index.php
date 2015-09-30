4dce69c01a6f70f1b790f114fa577d378a946c51
ayeo <me@ayeo.pl>
2015-09-30 22:52:21 +0200

    Clean and update readme

--- index.php
+++ index.php
@@ -3,17 +3,16 @@ use Ayeo\Barcode;
 
 require_once('vendor/autoload.php');
 
-//$builder = new Barcode\Builder();
-//$builder->setBarcodeType('gs1-128');
-//$builder->setFilename('barcode.png');
-//$builder->setImageFromat('png');
-//$builder->setWidth(500);
-//$builder->setHeight(150);
+$builder = new Barcode\Builder();
+$builder->setBarcodeType('gs1-128');
+$builder->setFilename('barcode.png');
+$builder->setImageFromat('png');
+$builder->setWidth(500);
+$builder->setHeight(150);
 //$builder->setFontPath('FreeSans.ttf');
-//$builder->setFontSize(15);
-//$builder->setBackgroundColor(255, 255, 255);
-//$builder->setPaintColor(0, 0, 0);
-//
-//$builder->output('(10)123456(400)11');
+$builder->setFontSize(15);
+$builder->setBackgroundColor(255, 255, 255);
+$builder->setPaintColor(0, 0, 0);
+$builder->output('(10)123456(400)11');
 
-Barcode\Builder::build()->setWidth(600)->setBackgroundColor(100, 100, 100)->output('(10)123456(400)11');
+//Barcode\Builder::build()->setWidth(600)->setBackgroundColor(100, 100, 100)->output('(10)123456(400)11');

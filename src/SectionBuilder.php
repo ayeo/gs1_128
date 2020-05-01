2f049bf5247874cf61bc225c827c02b25fb2b8bf
ayeo <me@ayeo.pl>
2020-05-01 03:23:35 +0200

    Add AI 240

--- src/SectionBuilder.php
+++ src/SectionBuilder.php
@@ -40,6 +40,8 @@ class SectionBuilder
         '12' => [6, 6, 'PAYMENT DATE (12) - must be in YYMMDD format'],
         '15' => [6, 6, 'BEST BEFORE DATE (YYMMDD) - must bi in YYMMDD format'],
         '37' => [1, 8, 'NUMBER OF UNITS CONTAINED (37) - must contains between 1-8 digits'],
+        '240' => [1, 15, 'ADDITIONAL PRODUCT IDENTIFICATION ASSIGNED BY THE MANUFACTURER (240) - must contains between 1-15 digits'
+        ],
         '3301' => [6, 6, 'CONTAINER GROSS WEIGHT (KG) - must contains 6 chars'],
 		'3900' => [1, 15, 'AMOUNT PAYABLE - SINGLE MONETARY AREA (3900) - must contains between 1-15 digits'],
 		'3901' => [1, 15, 'AMOUNT PAYABLE - SINGLE MONETARY AREA (3901) - must contains between 1-15 digits'],
